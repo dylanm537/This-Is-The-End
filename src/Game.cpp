e11769c87f467fa3f63739aac4d3d6d101f99ba0
Dylan McQuaker <mcquakerd@gmail.com>
2025-12-06 15:21:37 +0000

    Game, main cpp files commented

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,33 +1,39 @@
 #include "Game.hpp"
 #include <iostream>
-#include "MenuState.hpp"
+#include "MenuState.hpp" //included for sending initial state
 
+// constructor
 Game::Game() :
+    // creating sfml window with dimensions
     window(
         sf::VideoMode(Parameters::game_width, Parameters::game_height),
         "This Is The End?"
     )
 {
+    // makes framerate limited to 60 for 1080p@60fps
     window.setFramerateLimit(60);
+    // hides cursor because we have our own crosshair
     window.setMouseCursorVisible(false);
 
-   
+    // loads crosshair image
     if (!crosshairTex.loadFromFile("assets/crosshair.png")) {
-        std::cout << "FAILED TO LOAD CROSSHAIR\n";
+        std::cout << "crosshair didnt load\n";
     }
+    // makes the origin of the image in the center of it
     crosshair.setTexture(crosshairTex);
     crosshair.setOrigin(crosshairTex.getSize().x / 2, crosshairTex.getSize().y / 2);
     crosshair.setScale(0.1f, 0.1f);
 
-    
+    // sends initial state
     pushState(std::make_unique<MenuState>(Parameters::game_width, Parameters::game_height, "assets/arial.ttf"));
 }
 
+// adds a new state to the top of the state stack, new state will be the active one
 void Game::pushState(std::unique_ptr<State> state)
 {
     states.push(std::move(state));
 }
-
+// removes state from top of stack, the one beneath will be the active one
 void Game::popState()
 {
     if (!states.empty())
@@ -35,50 +41,52 @@ void Game::popState()
         states.pop();
     }
 }
-
+// changes the top state
 void Game::changeState(std::unique_ptr<State> state)
 {
     popState();
     pushState(std::move(state));
 }
 
+// main game loop
 void Game::run()
 {
-    sf::Clock clock;
+    sf::Clock clock; // time between frames dt
 
     while (window.isOpen())
     {
         sf::Event event;
-        while (window.pollEvent(event))
+        while (window.pollEvent(event)) // handles events
         {
             if (event.type == sf::Event::Closed)
                 window.close();
-
+            // passes event to state if there is a current state
             if (!states.empty())
             {
                 states.top()->handleInput(*this, event);
             }
         }
-
+        // updating
         if (!states.empty())
         {
             float dt = clock.restart().asSeconds();
             states.top()->update(*this, dt);
         }
-
+        // rendering
         if (!states.empty())
         {
-            window.clear(sf::Color::Black);
+            window.clear(sf::Color::Black); // clears window black
+            // renders current active state
             states.top()->render(window);
 
-
+            // draws crosshair
             sf::Vector2i mp = sf::Mouse::getPosition(window);
             crosshair.setPosition((float)mp.x, (float)mp.y);
             window.draw(crosshair);
 
             window.display();
         }
-        else 
+        else // if no states in the stack
         {
             window.close();
         }
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include "Game.hpp"
-
+// main running point of application
 int main()
 {
+    //creates instance of game class
     Game game;
+    // starts main game loop
     game.run();
+    // termination
     return 0;
 }
